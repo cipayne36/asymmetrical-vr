63b3a3a2a7c25ad0689261a9d29591cc0512f45d
root <root@LAPTOP-0STCVJPS.localdomain>
2018-03-04 15:48:47 -0500

    initial commit, easy rtc setup, basic networked-aframe example

---
+++ script.js
@@ -0,0 +1,11 @@
+function generateMaze() {
+	//how do we generate a maze?
+}
+
+function serverConnect() {
+	//how do we connect to a server
+}
+
+function detectSurface() {
+	//do we want to detect a surface for the maze, after class suggestion?
+}
