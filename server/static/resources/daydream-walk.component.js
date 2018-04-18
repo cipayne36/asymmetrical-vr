01e82218e361cf9b0f4531f371ced99eaab90879
MKatwood <mckenzie@mkatwood.com>
2018-04-17 21:58:17 -0400

    added directional input on daydream controller

--- server/static/resources/daydream-walk.component.js
+++ server/static/resources/daydream-walk.component.js
@@ -1,12 +1,20 @@
 AFRAME.registerComponent('daydream-walk', {
 
-
 	init: function() {
+		isPressed = false;
+
 		console.log("Daydream-walk component is loaded");
+		document.addEventListener("trackpadtouchstart", function() {
+			isPressed = true;
+		});
+		document.addEventListener("trackpadtouchend", function() {
+			isPressed = false;
+		});
+	},
 
-		document.addEventListener("click", function() {
+	tick: function() {
+		if (isPressed) {
 			var player = document.getElementById("player");
-
 			var new_x = 0;
 			var new_z = 0;
 			var x =	player.object3D.position.x;
@@ -19,7 +27,8 @@ AFRAME.registerComponent('daydream-walk', {
 			var a = new THREE.Vector3(-new_x, 0,-new_z);
 			player.object3D.position.add(a);
 			console.log("R: " + player.object3D.rotation.y + "      X: " + new_x + "     Z: " + new_z);
-
-		});
+		}
 	}
+
+
 });
